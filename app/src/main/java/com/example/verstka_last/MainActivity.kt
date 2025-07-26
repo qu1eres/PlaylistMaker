769382db85f625c2e1decabb72f25d5c47dfa935
quieres <qu1eres@yandex.ru>
2025-07-26 12:30:39 +0300

    1

--- app/src/main/java/com/example/verstka_last/MainActivity.kt
+++ app/src/main/java/com/example/verstka_last/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.verstka_last
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 
